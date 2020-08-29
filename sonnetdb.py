19bc47b66891e7e904a886fc7ba4ed138745f745
AnabelSalomone <anabel.salomone@gmail.com>
2020-08-29 20:12:27 +0200

    WIP

--- sonnetdb.py
+++ sonnetdb.py
@@ -3,29 +3,23 @@ import sqlite3
 conn = sqlite3.connect('sonnets.sqlite')
 cur = conn.cursor()
 
+cur.execute('DROP TABLE IF EXISTS Sonnets')
 cur.execute('CREATE TABLE Sonnets (verse TEXT, count INTEGER)')
 
-file_exists = False
 file_ext = ".txt"
+count = 0
 
-while not file_exists:
-  fname_input = input('Enter Poem Name: ')
-  fname = fname_input.replace(" ", "") + file_ext
-  try:
-    with open(fname) as fhandle:
-      for line in fhandle:
-        if not line.startswith('Love'): continue
-        count += 1
-        verse = line.rstrip()
-        cur.execute('INSERT INTO Sonnets (verse, count) VALUES (?, ?)', (line, count,))
-        conn.commit()
+fname_input = input('Enter Poem Name: ')
+fname = fname_input.replace(" ", "") + file_ext
+with open(fname) as fhandle:
+  for line in fhandle:
+    if not line.startswith('Love'): continue
+    count =+ 1
+    verse = line.rstrip()
+    cur.execute('INSERT INTO Sonnets (verse, count) VALUES (?, ?)', (line, count,))
+    conn.commit()
 
-        sqlval = 'SELECT verse, count FROM Sonnets ORDER BY count'
+    sqlval = 'SELECT verse, count FROM Sonnets ORDER BY count'
 
-        for i, row in cur.execute(sqlval):
-          print(str(row[i]))
-    file_exists = True
-  except Exception:
-    print("File not found")
-    file_exists = False
-
+    for row in cur.execute(sqlval):
+      print(str(row))
