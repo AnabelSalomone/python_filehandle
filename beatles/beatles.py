ad4f0a06718ce76386555deb8ccde9d76576c624
AnabelSalomone <anabel.salomone@gmail.com>
2020-09-03 20:09:57 +0200

    json to db

--- beatles/beatles.py
+++ beatles/beatles.py
@@ -7,20 +7,19 @@ cur = conn.cursor()
 cur.executescript('''
 DROP TABLE IF EXISTS Album;
 
---sql
 CREATE TABLE Album (
-  id INTEGER NOT NULL PRIMARY KEY
-  name TEXT UNIQUE
-  cover TEXT 
-
-)
-
+  id INTEGER NOT NULL PRIMARY KEY,
+  name TEXT UNIQUE,
+  cover TEXT
+); 
 ''')
 
 with open('tracks.json') as fhandle:
   dictionary = json.load(fhandle) #Deserialize fp (a .read()-supporting file-like object containing a JSON document) to a Python object
 
-
-  cur.executescript
   for item in dictionary:
-    print(item["name"])
+    item_id = item["id"]
+    name = item["name"]
+    cover = item["cover"]
+    cur.execute('INSERT INTO Album (id, name, cover) VALUES (?, ?, ?)', (item_id, name, cover,))
+    conn.commit()
